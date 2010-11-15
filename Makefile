2106d11e4cdfae780c917e313471cca707d28103
Attila Tajti <attila.tajti@xmta.eu>
2010-11-15 09:41:25 +0100

    wip

--- Makefile
+++ Makefile
@@ -2,19 +2,13 @@ include $(GOROOT)/src/Make.inc
 
 TARG=gobasicsound
 
-GOFILES_darwin=
-CGOFILES_darwin=gobasicsound_darwin.go
-CGO_LDFLAGS_darwin+=basicsound_darwin.o -framework Cocoa
-CGO_DEPS_darwin+=basicsound_darwin.o
+CGOFILES=gobasicsound_darwin.go
+CGO_LDFLAGS=basicsound_darwin.o -framework Cocoa
+CGO_DEPS=basicsound_darwin.o
 
 GOFILES_windows=gobasicsound_windows.go
 CGOFILES_windows=
 
-CGO_LDFLAGS=$(CGO_LDFLAGS_$(GOOS))
-CGO_DEPS=$(CGO_LDFLAGS_$(GOOS))
-
-CGOFILES+=$(CGOFILES_$(GOOS))
-GOFILES+=$(GOFILES_$(GOOS))
 
 include $(GOROOT)/src/Make.pkg
 
--- basicsound_darwin.m
+++ basicsound_darwin.m
@@ -1,11 +1,95 @@
 #import <Cocoa/Cocoa.h>
 #include <pthread.h>
 
+@class AudioQueue;
+
+@interface AudioJob : NSObject
+{
+	NSCondition *finishedSignal;
+}
+
+- (void)addToQueue:(AudioQueue *)aQueue;
+
+@end
+
+@implementation AudioQueue
+- (void)addToQueue:(AudioQueue *)aQueue
+{
+	[self
+		performSelector:@selector(performIterationAndRequeueInJobQueue:)
+		onThread:runLoopThread 
+		withObject:self
+		waitUntilDone:NO];
+}
+
+@end
+
+@interface AudioQueue : NSObject
+{
+	NSThread *runLoopThread;
+	BOOL terminated;
+}
+
+- (void)add:(AudioJob *)aJob;
+@end
+
+@implementation AudioQueue
+
+- (id)init
+{
+	self = [super init];
+	if (self != nil)
+	{
+		runLoopThread = [[NSThread alloc] initWithTarget:self selector:@selector(runLoop) object:nil];
+		[runLoopThread start];
+	}
+	return self;
+}
+
+- (void)runLoop
+{
+	//
+	// Run loops will exit if they have no sources. We add a dummy NSPort object
+	// to the run loop so it will run until we call CFRunLoopStop().
+	//
+	NSPort *dummyPort = [[NSMachPort alloc] init];
+	[[NSRunLoop currentRunLoop]
+		addPort:dummyPort
+		forMode:NSDefaultRunLoopMode];
+		
+	CFRunLoopRun();
+	
+	[[NSRunLoop currentRunLoop]
+		removePort:dummyPort
+		forMode:NSDefaultRunLoopMode];
+	[dummyPort release];
+}
+
+- (void)add:(AudioJob *)aJob
+{
+	[aJob
+		performSelector:@selector(performIterationAndRequeueInJobQueue:)
+		onThread:runLoopThread 
+		withObject:self
+		waitUntilDone:NO];
+}
+
+- (void)dealloc
+{
+	[runLoopThread release];
+	runLoopThread = nil;
+	[super dealloc];
+}
+
+
+@end
+
 pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
 NSSound* activeSound;
 
 void basicsound_init()
 {
+	[[NSAutoreleasePool alloc] init];
 }
 
 void basicsound_play(const char* filename, int loop)
@@ -19,6 +103,8 @@ void basicsound_play(const char* filename, int loop)
 	[activeSound setLoops: (BOOL)loop];
 	[activeSound play];
 	pthread_mutex_unlock(&mtx);
+
+	[[NSRunLoop currentRunLoop] run];
 }
 
 void basicsound_stop()
