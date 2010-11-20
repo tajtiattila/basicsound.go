af1cc2f0ff7bcc758ada18cfcb4aac593775f568
Attila Tajti <attila.tajti@xmta.eu>
2010-11-20 10:17:02 +0100

    test wav added

--- basicsound_test.go
+++ basicsound_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestBasicSound(t *testing.T) {
 	for i := int(0); i < 2; i++ {
-		Play("/Applications/Mail.app/Contents/Resources/Mail Sent.aiff",true)
+		Play("buzz.wav",true)
 		time.Sleep(5e9)
 		Stop()
 		time.Sleep(1e9)
