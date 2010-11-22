e10607b129e691c7b71b24cfabe429aa82ced837
Attila Tajti <attila.tajti@xmta.eu>
2010-11-22 10:25:12 +0100

    shortened test

--- basicsound_test.go
+++ basicsound_test.go
@@ -8,8 +8,8 @@ import (
 func TestBasicSound(t *testing.T) {
 	for i := int(0); i < 2; i++ {
 		Play("buzz.wav",true)
-		time.Sleep(5e9)
+		time.Sleep(5e8)
 		Stop()
-		time.Sleep(1e9)
+		time.Sleep(1e8)
 	}
 }
